6b6ec331e7a218230119149bc91c9783885ec9a1
Cobord <ammar.s.husain@gmail.com>
2024-04-09 00:09:43 -0400

    refactor

--- poker_scoring_api.py
+++ poker_scoring_api.py
@@ -1,3 +1,8 @@
-# TODO: Implement poker scoring api here
-# def score_poker_hands():
-#    pass
+"""
+none
+"""
+def score_poker_hands(_hand1_str : str, _hand2_str: str):
+    """
+    none
+    """
+    return True
--- spoiler_alert_keep_out/level_documentation.py
+++ spoiler_alert_keep_out/level_documentation.py
@@ -1,33 +1,26 @@
-from datetime import datetime, timedelta
-import random
+"""
+TODO
+"""
+from spoiler_alert_keep_out.rules_spec import RulesSpec, display_the_rules
+from spoiler_alert_keep_out.boss_email_general import BossEmailTemplating
 from spoiler_alert_keep_out.boss_emails import boss_emails
 from spoiler_alert_keep_out.poker_rules import poker_rules
 
-
-def procedural_datetime(level):
-    random.seed(45)
-    email_time = datetime(2021, 10, 11, 9, 45)
-    for i in range(level):
-        proc_minutes = random.randrange(45, 65)
-        email_time += timedelta(minutes=proc_minutes)
-    return email_time.strftime("%b %d, %Y, %I:%M %p")
-
+poker_boss = BossEmailTemplating("Poker", "the-bawss@instapoker.com")
 
 def display_boss_email(level):
-    subject = "Poker" if level == 0 else "re: Poker"
-    datetime_str = procedural_datetime(level)
+    """
+    the boss's email at current level
+    """
+    poker_boss.print_email_headers(level)
     if level >= len(boss_emails):
         level = len(boss_emails) - 1
-    print()
-    print("from:    the-bawss@instapoker.com")
-    print("date:    {}".format(datetime_str))
-    print("subject: {}".format(subject))
     print(boss_emails[level])
-    # else:
-    #     print("(No Boss Email available for Level {})".format(level))
 
-
-def display_test_level_info(level, submit):
+def display_test_level_info(level : int, submit):
+    """
+    TODO
+    """
     if not submit:
         print("(Running your local tests only)")
     if level == 0:
@@ -35,28 +28,14 @@ def display_test_level_info(level, submit):
     elif level == 1:
         print("(Running Incremental Tests for level 1)")
     elif level > 1:
-        print("(Running Incremental Tests for levels 1 - {})".format(level))
+        print(f"(Running Incremental Tests for levels 1 - {level})")
 
+parsed_poker_rules = list(map(lambda raw: RulesSpec.parse(raw,len(boss_emails)),poker_rules))
 
-def display_poker_rules(level):
-    display_rules = list()
-    for r in poker_rules:
-        rule_level = r.get("level")
-        rule_level_range = r.get("level_range")
-
-        if rule_level and rule_level > level:
-            continue
-
-        if rule_level_range:
-            rule_min, rule_max = rule_level_range
-            if rule_min > level or rule_max < level:
-                continue
-
-        display_rules.append(r)
-
-    display_rules.sort(key=lambda x: x.get("sort_order"), reverse=True)
-
+def display_poker_rules(level : int):
+    """
+    display the rules text at current level
+    """
     print()
-    for r in display_rules:
-        print(r.get("text", "").strip())
-        print()
+    print(display_the_rules(parsed_poker_rules,level))
+    print()
